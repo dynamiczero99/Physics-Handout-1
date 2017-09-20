df2663e8cc990e7f90f84eb5bee7524b580c93e7
Joel <dynamic.cast99@gmail.com>
2017-09-20 10:32:43 +0200

    0.1

---
+++ ModulePhysics.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "Module.h"
+#include "Globals.h"
+
+#define PIXELS_PER_METER 50.0f
+#define METERS_PER_PIXEL 0.02f
+
+#define METERS_TO_PIXELS(m) ((int) floor(PIXELS_PER_METER * m))
+#define PIXELS_TO_METERS(p) ((float) METERS_PER_PIXEL * p)
+
+class b2World;
+
+class ModulePhysics : public Module
+{
+public:
+	ModulePhysics(Application* app, bool start_enabled = true);
+	~ModulePhysics();
+
+	bool Start();
+	update_status PreUpdate();
+	update_status PostUpdate();
+	bool CleanUp();
+	
+
+
+private:
+	b2World* za_warudo;
+	bool debug;
+};
